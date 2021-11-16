bc2aab15480667ff528a9e5f16af8e37d3441213
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2021-11-16 12:52:05 +0200

    get meals info from API

---
+++ src/api.js
@@ -0,0 +1,22 @@
+import likebtn from './img/heart.png'
+const mealapiurl = 'https://www.themealdb.com/api/json/v1/1/search.php?f=e';
+const meallist = document.querySelector('.meal-list');
+const getmeals = async () => {
+ 
+  const meals = await fetch(mealapiurl)
+      .then(res => res.json())
+      .then(data  =>  data.meals); 
+
+  meals.forEach(element => {
+    const newmeal =  document.createElement('li');
+    
+    newmeal.innerHTML = `<h4>${element.strMeal}</h4>
+            <img src="${element.strMealThumb}">;
+            <img src='${likebtn}'>`;
+            
+    meallist.appendChild(newmeal);
+});
+return meals;
+
+}
+getmeals();
