99493a5b781b981a75722963aab690d63e8410d2
shoiraTa <shoira.shakirovna@gmail.com>
2021-11-16 16:57:06 +0500

    Add: styles

--- src/api.js
+++ src/api.js
@@ -1,5 +1,3 @@
-import likebtn from './img/heart.png';
-
 const mealapiurl = 'https://www.themealdb.com/api/json/v1/1/search.php?f=e';
 const meallist = document.querySelector('.meal-list');
 const getmeals = async () => {
@@ -10,9 +8,20 @@ const getmeals = async () => {
   meals.forEach((element) => {
     const newmeal = document.createElement('li');
 
-    newmeal.innerHTML = `<h4>${element.strMeal}</h4>
-            <img src="${element.strMealThumb}">;
-            <img src='${likebtn}'>`;
+    newmeal.innerHTML = ` 
+    <div class="meal">
+    <div class="meal-header">
+      <img src="${element.strMealThumb}" alt="${element.strMeal}">
+    </div>
+    <div class="meal-body d-flex justify-between">
+      <h4>${element.strMeal}</h4>
+      <button class="fav-btn"><i class="fas fa-heart "></i></button>
+    </div>
+    <div>
+      <div class = "likes"><span class= "likes-qty"> </span> likes </div>
+      <button class= "comments">Comments</button>
+    </div>
+  </div>`;
 
     meallist.appendChild(newmeal);
   });
