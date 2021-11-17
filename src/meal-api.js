9fd05cabd03f3fb5a1528a7b0e908609641ee830
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2021-11-17 15:17:28 +0200

    Fix: Linter Errors

--- src/meal-api.js
+++ src/meal-api.js
@@ -14,39 +14,39 @@ const popup = async () => {
     meals.forEach((element) => {
       if (element.idMeal === id) {
         modal.innerHTML = `
-        <div class="modal">
-        <div class="meal-header justify-end d-flex">
-            <button class="close"><i class="fas fa-times"></i></button>
-        </div>
-        <div class="modal-header-img d-flex ">
-            <img class="modal-img" src="${element.strMealThumb}" alt="${element.strMeal}">
-            <h2 class="titme">${element.strMeal}</h2>
-        </div>
-        <div class="modal-description d-flex ">
-        <div class = "left">
-        <p> <span class="description-header">Category:</span> ${element.strCategory} <p>
-        <p> <span class="description-header">Ingredients:</span>
-        <ul id = "ingredientsUl">
-        </ul>
-        </span><p>
-        </div>
-        <div class = "right">
-        <p> <span class="description-header">Country:</span> ${element.strArea} <p>
-        <p> <span class="description-header">Instructions:</span> <p> <span class = "recipe-instrruction"> ${element.strInstructions}  > read more </button> </span>
-        </div>
-    </div>
-      `;
+          <div class="modal">
+          <div class="meal-header justify-end d-flex">
+              <button class="close"><i class="fas fa-times"></i></button>
+          </div>
+          <div class="modal-header-img d-flex ">
+              <img class="modal-img" src="${element.strMealThumb}" alt="${element.strMeal}">
+              <h2 class="titme">${element.strMeal}</h2>
+          </div>
+          <div class="modal-description d-flex ">
+          <div class = "left">
+          <p> <span class="description-header">Category:</span> ${element.strCategory} <p>
+          <p> <span class="description-header">Ingredients:</span>
+          <ul id = "ingredientsUl">
+          </ul>
+          </span><p>
+          </div>
+          <div class = "right">
+          <p> <span class="description-header">Country:</span> ${element.strArea} <p>
+          <p> <span class="description-header">Instructions:</span> <p> <span class = "recipe-instrruction"> ${element.strInstructions}  > read more </button> </span>
+          </div>
+      </div>
+        `;
         const ingredientsUl = document.querySelector('#ingredientsUl');
         const ingredients = () => {
           const entries = Object.entries(element);
           const ingredientsArray = entries
             .filter(([key, value]) => key.startsWith('strIngredient') && value && value.trim())
-            /* eslint-disable-next-line */
-            .map(([key, value]) => value);
+          /* eslint-disable-next-line */
+              .map(([key, value]) => value);
           const measuresArray = entries
             .filter(([key, value]) => key.startsWith('strMeasure') && value && value.trim())
-            /* eslint-disable-next-line */
-            .map(([key, value]) => value);
+          /* eslint-disable-next-line */
+              .map(([key, value]) => value);
           for (let i = 1; i < ingredientsArray.length; i += 1) {
             ingredientsUl.innerHTML += `<li> ${ingredientsArray[i]} - ${measuresArray[i]} </li> `;
           }
@@ -62,7 +62,7 @@ const popup = async () => {
       }
     });
   };
-  // popupModalDataSet(52895);
+    // popupModalDataSet(52895);
 
   for (let i = 0; i < btn.length; i += 1) {
     btn[i].addEventListener('click', () => {
@@ -86,40 +86,22 @@ const getmeals = async () => {
       meals.forEach((element) => {
         const newmeal = document.createElement('li');
         newmeal.innerHTML = ` 
-        <div class="meal">
-        <div class="meal-header">
-          <img src="${element.strMealThumb}" alt="${element.strMeal}">
-        </div>
-        <div class="meal-body d-flex justify-between">
-          <h4>${element.strMeal}</h4>
-          <button class="fav-btn" ><i class="fas fa-heart"></i></button>
-        </div>
-        <div>
-          <div class = "likes"><span class= "likes-qty"> </span> likes </div>
-          <button class= "comments" data="${element.idMeal}">Comments</button>
-        </div>
-      </div>`;
+          <div class="meal">
+          <div class="meal-header">
+            <img src="${element.strMealThumb}" alt="${element.strMeal}">
+          </div>
+          <div class="meal-body d-flex justify-between">
+            <h4>${element.strMeal}</h4>
+            <button class="fav-btn" ><i class="fas fa-heart"></i></button>
+          </div>
+          <div>
+            <div class = "likes"><span class= "likes-qty"> </span> likes </div>
+            <button class= "comments" data="${element.idMeal}">Comments</button>
+          </div>
+        </div>`;
         meallist.appendChild(newmeal);
       });
     };
-
-
-  meals.forEach((element) => {
-    const newmeal = document.createElement('li');
-    newmeal.innerHTML = ` 
-    <div class="meal">
-    <div class="meal-header">
-      <img src="${element.strMealThumb}" alt="${element.strMeal}">
-    </div>
-    <div class="meal-body d-flex justify-between">
-      <h4>${element.strMeal}</h4>
-      <button class="fav-btn"><i class="fas fa-heart "></i></button>
-    </div>
-    <div>
-      <div class = "likes"><span class= "likes-qty"> </span> likes </div>
-      <button class= "comments">Comments</button>
-    </div>
-  </div>`;
 
     const mealapiurl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
     const meals = await fetch(mealapiurl)
@@ -136,7 +118,6 @@ const getmeals = async () => {
     }
   };
 
-
   for (let i = 0; i < links.length; i += 1) {
     links[i].addEventListener('click', () => {
       const category = links[i].textContent.toLowerCase();
@@ -146,3 +127,6 @@ const getmeals = async () => {
     });
   }
   getCategoryUrl('seafood');
+};
+
+getmeals();
